2aca3cb42560011e90b20e8764a866535f69e25a
halurabiyyu <halurabiyyu@gmail.com>
2024-02-20 10:23:40 +0700

    Routing

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,4 @@
 <?php
-
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +15,39 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/hello', function(){
+    return 'Hello World';
+});
+
+Route::get('/world', function(){
+    return 'World';
+});
+
+Route::get('/', function(){
+    return 'Selamat Datang';
+});
+
+Route::get('/about', function(){
+    return 'Nama : Halur Muhammad Abiyyu, NIM : 2141720072';
+});
+
+Route::get('/user/{name}', function ($name){
+    return 'Nama Saya ' . $name;
+});
+
+Route::get('/posts/{post}/comments/{comment}', function($postId, $commentId){
+    return 'Pos ke-' . $postId . ' Komentar ke-: ' . $commentId;
+});
+
+Route::get('/articles/{id}', function($id){
+    return 'Halaman Artikel dengan ID ' . $id;
+});
+
+Route::get('/user/{name?}', function($name='John'){
+    return 'Nama Saya ' . $name;
+});
+
+Route::get('/user/profile', function(){
+    return "Profile";
+}) -> name('profile');
